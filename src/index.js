de5a3200446d531c6816fca6e8cce7f203fc951c
Facundo Alavar <ezequielalavar2@gmail.com>
2024-11-24 14:56:48 -0300

    Se agregan permisos de cors

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import usersRoutes from "./routes/users.routes.js";
 import morgan from "morgan";
 import { PORT } from "./config.js";
+import cors from "cors";
 
 const app = express();
 
@@ -12,6 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(usersRoutes);
+app.use(cors({ origin: "http://localhost:4200" }));
 
 app.listen(PORT);
 // eslint-disable-next-line no-console
