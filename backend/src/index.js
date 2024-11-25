2baebd6f349503eca21ce3824fb9b33256c797ad
ezealavar <ezequielalavar2@gmail.com>
2024-11-25 00:57:24 -0300

    Se agrega el proxy, el nodo maestro, prometheus, grafana, el cliente y el exporter al docker compose

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,8 @@ app.use(morgan("dev"));
 
 // middlewares
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:4200" }));
+// app.use(cors({ origin: "http://localhost:4200" }));
+app.use(cors());
 
 app.use(express.urlencoded({ extended: false }));
 
